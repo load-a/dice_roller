d3696df7b4cc13308f289e45145fba3b02ce7ba0
Load A <143745153+load-a@users.noreply.github.com>
2023-10-25 17:08:20 -0400

    Initial Build

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in dice_roller.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/dice_roller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dice_roller/version"
+
+module DiceRoller
+  class Error < StandardError; end
+  # Your code goes here...
+end
